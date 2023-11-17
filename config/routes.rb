4a796406c67a43aa2f0228bb509b9714c6856840
umarazhar1 <umarazhar.ua1122@gmail.com>
2023-11-17 22:04:24 +0500

    Added new.html.erb view file and defined new and create actions in articles_controller.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
   # Defines the root path route ("/")
   # root "articles#index"
