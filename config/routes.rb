84988fcf4b0ea74c60fd4ba061c0d572ac2de35c
Umar Azhar <148058627+umarazhar1@users.noreply.github.com>
2023-12-03 01:58:08 +0500

    Added login form, create and destroy session of users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
