68b92235cae627f6ba7196dd965718bfb8d4a105
Umar Azhar <148058627+umarazhar1@users.noreply.github.com>
2023-12-03 02:40:18 +0500

    Added authentication helper methods and helper methods in application_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,8 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-				@article.user = User.first    #added temporarily
+				# @article.user = User.first    #added temporarily
+				@article.user = current_user
         if @article.save
 					flash[:notice] = "Article was created successfully!"
         	redirect_to article_path(@article)
